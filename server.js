2cd121e6a0fb3cb40afa38d7595d90f558b9ab85
saqib.abbas <saqib.abbas@tenpearls.com>
2019-07-06 15:54:04 +0500

    node fixes

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./'));
+app.use(express.static(__dirname + '/dist/myng'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/src/index.html'));
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/myng/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
