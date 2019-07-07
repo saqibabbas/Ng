9a4af2bb540da3531a5e0b8478cba3f26522e9b1
saqib.abbas <saqib.abbas@tenpearls.com>
2019-07-07 11:11:05 +0500

    build path changed

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/myng'));
+app.use(express.static(__dirname + '/dist'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/myng/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
